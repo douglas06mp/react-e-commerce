66723c8b27131cf954d7301498a166bd3582c35a
douglas <douglas06mp@gmail.com>
2020-05-18 23:46:38 +0800

    move firebase config to private file

--- src/firebase/firebase.util.js
+++ src/firebase/firebase.util.js
@@ -1,17 +1,8 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import { config } from './firebase.config';
 
-const config = {
-  apiKey: 'AIzaSyBqKsQWzY1NKAslGSMzN6Qw2ko6VLg96a0',
-  authDomain: 'react-e-commerce-e170c.firebaseapp.com',
-  databaseURL: 'https://react-e-commerce-e170c.firebaseio.com',
-  projectId: 'react-e-commerce-e170c',
-  storageBucket: 'react-e-commerce-e170c.appspot.com',
-  messagingSenderId: '409610674317',
-  appId: '1:409610674317:web:1a2c61ce3accf56c2902c4',
-  measurementId: 'G-ZJFJHTMCNB',
-};
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
