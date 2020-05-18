28702b14af0b5045b2f1a2654cb28d9a0d98c14f
douglas <douglas06mp@gmail.com>
2020-05-18 15:15:35 +0800

    amend style and add directory data

--- src/components/directory/Directory.js
+++ src/components/directory/Directory.js
@@ -1,46 +1,13 @@
 import React, { Component } from 'react';
-import '../category/Category';
-import './Directory.scss';
 import Category from '../category/Category';
+import DIRECTORY_DATA from './directory.data';
+import './Directory.scss';
 
 export default class Directory extends Component {
   constructor() {
     super();
     this.state = {
-      category: [
-        {
-          title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-          linkUrl: 'hats',
-          id: 1,
-        },
-        {
-          title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-          linkUrl: 'jackets',
-          id: 2,
-        },
-        {
-          title: 'shoes',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-          linkUrl: 'shoes',
-          id: 3,
-        },
-        {
-          title: 'womens',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-          linkUrl: 'womens',
-          size: 'large',
-          id: 4,
-        },
-        {
-          title: 'mens',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-          linkUrl: 'mens',
-          size: 'large',
-          id: 5,
-        },
-      ],
+      category: DIRECTORY_DATA,
     };
   }
   render() {
--- src/pages/shop/Shop.js
+++ src/pages/shop/Shop.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import CollectionPreview from '../../components/collection-preview/CollectionPreview';
 import SHOP_DATA from './shop.data';
+import './Shop.scss';
 
 export default class Shop extends Component {
   constructor(props) {
