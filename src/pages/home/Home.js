59121a33bb60b7ca9fc3a2fb1e483272465294d5
douglas <douglas06mp@gmail.com>
2020-05-22 17:32:44 +0800

    remove Home folder from git history and add it back with name home

---
+++ src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Directory from '../../components/directory/Directory';
+import './Home.scss';
+
+const Home = () => {
+  return (
+    <div className="home">
+      <Directory />
+    </div>
+  );
+};
+
+export default Home;
