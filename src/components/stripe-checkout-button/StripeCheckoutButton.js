c482d3aca5fabe3bd9ef5ab5d5d4df19b3707a71
douglas <douglas06mp@gmail.com>
2020-05-22 12:59:56 +0800

    integrate stripe payment with react-stripe-checkout and update title, favicon

---
+++ src/components/stripe-checkout-button/StripeCheckoutButton.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { publishKey } from '../../stripe';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="React App"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      locale="us"
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
